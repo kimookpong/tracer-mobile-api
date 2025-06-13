b6d501aec5d7b7fbd42cbc01ec992b80fe56c9d2
Hakim Mudor <78400565+kimookpong@users.noreply.github.com>
2025-06-13 15:34:12 +0700

    update

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.post("/admins", async (req, res) => {
   }
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });

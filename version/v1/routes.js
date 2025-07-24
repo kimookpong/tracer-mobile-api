76564e72906582bf6631b51e41dcb4ef6fd7e357
Hakim Mudor <78400565+kimookpong@users.noreply.github.com>
2025-07-24 10:09:23 +0700

    Add news routes to API
    Introduced GET endpoints for fetching all news and news by ID. These routes are protected by the verifyToken middleware and use the News model.

--- version/v1/routes.js
+++ version/v1/routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const Admins = require("./models/Admins");
 const Authen = require("./models/Authen");
 const User = require("./models/User");
+const News = require("./models/News");
 const verifyToken = require("../../middleware/auth");
 
 // Authentication
@@ -19,5 +20,9 @@ router.get("/users/:id", verifyToken, User.getId);
 // Admins
 router.get("/admins", verifyToken, Admins.getAll);
 router.get("/admins/:id", verifyToken, Admins.getId);
+
+// News
+router.get("/news", verifyToken, News.getAll);
+router.get("/news/:id", verifyToken, News.getId);
 
 module.exports = router;

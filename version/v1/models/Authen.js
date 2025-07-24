e796f455bc60d54d163639a37e1fe38b64728fec
Hakim Mudor <78400565+kimookpong@users.noreply.github.com>
2025-07-24 09:49:10 +0700

    Update Authen.js

--- version/v1/models/Authen.js
+++ version/v1/models/Authen.js
@@ -60,7 +60,7 @@ exports.login = async (req, res, next) => {
     res.status(200).json({
       status: "success",
       message: "Login successful",
-      data: data,
+      data: user,
       accessToken: accessToken,
       userId: user.id,
     });
